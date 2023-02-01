8702279db8e6b4a58683eecd0f0dafdf9af9fbe9
muratahika <82791212+muratahika@users.noreply.github.com>
2023-02-01 10:24:27 +0900

    グラフ(失敗)

--- javascript/glaph.js
+++ javascript/glaph.js
@@ -1,85 +1,139 @@
-let ctx = document.getElementById("chart").getContext('2d');
+var data = [{
+    name:"13-11",
+    value: 9
+},{
+    name:"14-11",
+    value: 4
+},{
+    name:"15-11",
+    value: 9
+},{
+    name:"16-11",
+    value: 5
+},{
+    name:"17-11",
+    value: 7
+},{
+    name:"18-11",
+    value: 11
+},{
+    name:"19-11",
+    value: 6
+},{
+    name:"20-11",
+    value: 13
+},{
+    name:"21-11",
+    value: 4
+},{
+    name:"22-11",
+    value: 4
+},{
+    name:"23-11",
+    value: 8
+},{
+    name:"24-11",
+    value: 7
+},{
+    name:"25-11",
+    value: 8
+},{
+    name:"26-11",
+    value: 9
+},{
+    name:"27-11",
+    value: 7
+},{
+    name:"28-11",
+    value: 5
+},{
+    name:"29-11",
+    value: 4
+},{
+    name:"30-11",
+    value: 6
+},{
+    name:"01-12",
+    value: 10
+},{
+    name:"02-12",
+    value: 6
+},{
+    name:"03-12",
+    value: 6
+},{
+    name:"04-12",
+    value: 7
+},{
+    name:"05-12",
+    value: 4
+}];
 
-var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
-gradientStroke.addColorStop(0, "#ff6c00");
-gradientStroke.addColorStop(1, "#ff3b74");
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    padding = 30,
+    width = 1080 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var gradientBkgrd = ctx.createLinearGradient(0, 100, 0, 400);
-gradientBkgrd.addColorStop(0, "rgba(244,94,132,0.2)");
-gradientBkgrd.addColorStop(1, "rgba(249,135,94,0)");
+var canvas = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .append("g");
 
-let draw = Chart.controllers.line.prototype.draw;
-Chart.controllers.line = Chart.controllers.line.extend({
-    draw: function() {
-        draw.apply(this, arguments);
-        let ctx = this.chart.chart.ctx;
-        let _stroke = ctx.stroke;
-        ctx.stroke = function() {
-            ctx.save();
-            //ctx.shadowColor = 'rgba(244,94,132,0.8)';
-            ctx.shadowBlur = 8;
-            ctx.shadowOffsetX = 0;
-            ctx.shadowOffsetY = 6;
-            _stroke.apply(this, arguments)
-            ctx.restore();
-        }
-    }
-});
+var x = d3.scale.ordinal()
+    .domain(data.map(function(d) {return d.name; }))
+    .rangeRoundBands([50, width + 10], 0.5);
+
+var y = d3.scale.linear()
+    .domain([0, d3.max(data, function(d) { return d.value; })])
+    .range([height - padding, padding])
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+canvas.append("g")
+    .attr("class","axis")
+    .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+    .call(xAxis)
 
 
+canvas.append("g")
+    .attr("class","axis")
+    .attr("transform", "translate(" + (margin.left) + ",0)")
+    .call(yAxis)
+    .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -40)
+        .attr("x", -25)
+        .style("text-anchor", "end")
+        .text("Afstand in km");
 
+canvas.selectAll("circle")
+    .data(data)
+    .enter()
+        .append("circle")
+        .attr({
+        cx: function (d) { return x(d.name)},
+        cy: function (d) { return y(d.value)},
+        r: 5,
+        class: "circle"
+    });
 
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+var link = d3.svg.line()
+    .x(function(d) { return x(d.name); })
+    .y(function(d) { return y(d.value); });
 
-    // The data for our dataset
-    data: {
-        labels: ["Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr"],
-        datasets: [{
-            label: "Income",
-            backgroundColor: gradientBkgrd,
-            borderColor: gradientStroke,
-            data: [5500, 2500, 10000, 6000, 14000, 1500, 7000,20000],
-            pointBorderColor: "rgba(255,255,255,0)",
-            pointBackgroundColor: "rgba(255,255,255,0)",
-            pointBorderWidth: 0,
-            pointHoverRadius: 8,
-            pointHoverBackgroundColor: gradientStroke,
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 4,
-            pointRadius: 1,
-            borderWidth: 5,
-            pointHitRadius: 16,
-        }]
-    },
+canvas.append("path")
+        .datum(data)
+        .attr("class", "link")
+        .attr("d", link);
 
-    // Configuration options go here
-    options: {
-    tooltips: {
-        backgroundColor:'#fff',
-        displayColors:false,
-        titleFontColor: '#000',
-        bodyFontColor: '#000'
+TweenLite.from("path", 5, {drawSVG:"0% 0%", ease:Linear.easeNone, delay:1} );
 
-        },
-    legend: {
-            display: false
-    },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    display:false
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    // Include a dollar sign in the ticks
-                    callback: function(value, index, values) {
-                        return (value / 1000) + 'K';
-                    }
-                }
-            }],
-        }
-    }
-});
+TweenMax.staggerFrom("circle", 1, {opacity: 0, ease:Power1.easeInOut},0.1);
